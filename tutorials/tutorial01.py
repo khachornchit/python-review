5f542227ffb6366ff74f53d3d2417f2f5871f418
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-08-04 16:19:39 +0700

    Update

--- tutorials/tutorial01.py
+++ tutorials/tutorial01.py
@@ -3,3 +3,9 @@ print("Hello universe")
 programming_languages = "Python", "Java", "C++", "C#"
 
 print(type(programming_languages))
+
+print(programming_languages)
+
+for language in programming_languages:
+    print(language)
+
