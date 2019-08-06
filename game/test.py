2215cd286fd853e3bace5f724c6509bcbd4215a7
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-08-07 00:05:35 +0700

    Update

---
+++ game/test.py
@@ -0,0 +1,11 @@
+game = [[2, 0, 1],
+        [1, 0, 1],
+        [2, 2, 1],]
+
+print("len(game) : ", len(game))
+print("range(len(game)) : ", range(len(game)))
+
+x = range(len(game))
+
+for i in x:
+    print(i)
--- game/win.py
+++ game/win.py
@@ -1,9 +1,50 @@
-game = [[2, 0, 1],
-        [2, 0, 0],
-        [2, 2, 0],]
+game = [[2, 2, 2],
+        [2, 2, 2],
+        [2, 2, 2],]
 
-check = []
+# Winner by rows
+def win_by_row():
+        for row in range(len(game)):
+                row_data = game[row]
+                win_horizental = False
+                for col in range(len(row_data)):
+                        if col > 0:
+                                if game[row][col] == game[row][col-1]:
+                                        win_horizental = True
+                                else:
+                                        win_horizental = False
+                                        break
+                if win_horizental == True:
+                        print("row ", row+1, " : ", "is the Winner !")
 
-for row in game:
-        check.append(row[0])
-        print(check.count(check[0]), " - ", len(check))
+# Winner by columns
+def win_by_col():
+        for col in range(len(game[0])):
+                for row in range(len(game)):
+                        win_vertical = False
+                        if row > 0:
+                                if game[row][col] == game[row-1][col]:
+                                        win_vertical = True
+                                else:
+                                        win_vertical = False
+                                        break
+                if win_vertical == True:
+                        print("Column ", col+1, " : ", "is the Winner !")
+
+# Winner by diagonal
+def win_by_diagonal():
+        row = 0
+        col = 0
+        if game[row][col] == game[row+1][col+1] == game[row+2][col+2]:
+                print("Diagonal forward is the Winner !")
+        row = 0
+        col = 2
+        if game[row][col] == game[row+1][col-1] == game[row+2][col-2]:
+                print("Diagonal reversed is the Winner !")
+
+def get_winner():   
+        win_by_row()
+        win_by_col()
+        win_by_diagonal()
+
+get_winner()
