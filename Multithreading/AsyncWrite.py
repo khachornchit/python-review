cf706e1136f8b0aa26c5c83c745ab78efa28e3af
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-08-13 00:17:23 +0700

    Multithreading, timer, lock, asynchronous

---
+++ Multithreading/AsyncWrite.py
@@ -0,0 +1,31 @@
+import threading
+import time
+
+
+class AsyncWrite(threading.Thread):
+    def __init__(self, text, out):
+        threading.Thread.__init__(self)
+        self.text = text
+        self.out = out
+
+    def run(self):
+        f = open(self.out, "a")
+        f.write(self.text + '\n')
+        f.close()
+        time.sleep(2)
+        print("Finished background file write to " + self.out)
+
+
+def Main():
+    message = input("Enter a string to store: ")
+    background = AsyncWrite(message, 'out.txt')
+    background.start()
+    print("The program can continue while it writes in another thread")
+    print("100 + 400 = ", 100+400)
+
+    background.join()
+    print("Waited until thread was complete")
+
+
+if __name__ == "__main__":
+    Main()
