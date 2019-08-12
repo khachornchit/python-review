d202ffb01fff9fef36dfa00d1c569bbaaa590d11
Khachornchit Songsaen <khachornchit.songsaen@gmail.com>
2019-08-13 03:49:52 +0700

    Networking, TCP, UDP

---
+++ Networking/TcpServer.py
@@ -0,0 +1,26 @@
+import socket
+
+
+def Main():
+    host = '127.0.0.1'
+    port = 5000
+
+    s = socket.socket()
+    s.bind((host, port))
+
+    s.listen(1)
+    c, addr = s.accept()
+    print("Connection from: " + str(addr))
+    while True:
+        data = c.recv(1024).decode('utf-8')
+        if not data:
+            break
+        print("From connected user: ", data)
+        data = data.upper()
+        print("Sending: " + data)
+        c.send(data.encode('utf-8'))
+    c.close()
+
+
+if __name__ == '__main__':
+    Main()
