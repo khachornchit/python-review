103982ae3fc8967c6a68eda281b7f4ec35c57551
Chorn <chorndev01@gmail.com>
2023-11-18 09:59:44 +0700

    update

--- advanced_concepts/numpy/zero_numpy.py
+++ advanced_concepts/numpy/zero_numpy.py
@@ -1,7 +1,18 @@
 import numpy as np
 
-print(np.zeros(5))
 
-print(np.zeros((3, 3)))
+# print(np.zeros(5))
+#
+# print(np.zeros((3, 3)))
+#
+# print(np.zeros((3, 3)).tolist())
 
-print(np.zeros((3, 3)).tolist())
+
+def create_zero_matrix(row, col):
+    return np.zeros((row, col), dtype=int)
+
+
+if __name__ == '__main__':
+    matrix = create_zero_matrix(5, 5)
+    matrix[4, 4] = 1
+    print(matrix)
