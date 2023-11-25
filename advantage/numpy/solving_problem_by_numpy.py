329f8c1a2a8b18128902950a0b326a95ae612001
Chorn <chorndev01@gmail.com>
2023-11-25 13:01:24 +0700

    update

--- advantage/numpy/solving_problem_by_numpy.py
+++ advantage/numpy/solving_problem_by_numpy.py
@@ -1,14 +1,10 @@
 import numpy as np
 
 
-def get_consensus(ss):
-    # define characters
-    chars = np.array(['A', 'C', 'G', 'T'])
-
+def get_consensus(chars, ss):
     # transform ss to numpy
     lines = np.array(ss.upper().split('\n'))
-    lines = lines[lines != '']
-    np1 = np.array([[*line.strip()] for line in lines])
+    np1 = np.array([[*line] for line in lines])
     rows_np1, cols_np1 = np1.shape
 
     # define count matrix
@@ -16,13 +12,22 @@ def get_consensus(ss):
 
     for i, char in enumerate(chars):
         char_count = np.char.count(np1, char)
-        result = np.sum(char_count, axis=0)
-        np_count[i] = result
+        output = np.sum(char_count, axis=0)
+        np_count[i] = output
 
     return np_count
 
 
+def final_output(chars, count):
+    max_values_per_column = np.max(count, axis=0)
+    print(max_values_per_column)
+
+
 if __name__ == '__main__':
+    chars = np.array(['A', 'C', 'G', 'T'])
     ss = "ATCGATGC\n" + "GTACaCGC\n" + "ACTACAGC\n" + "CtCAATTC\n" + "CTGgATTC"
-    count = get_consensus(ss)
-    print(count)
+
+    count = get_consensus(chars, ss)
+    final_output(chars, count)
+
+    # print(count)
