ae7fb8ecd78f837a27f994a13feddfe3a1a8ef9f
Chorn <chorndev01@gmail.com>
2023-11-25 19:59:43 +0700

    update numpy

--- advantage/numpy/solving_problem_by_numpy.py
+++ advantage/numpy/solving_problem_by_numpy.py
@@ -18,9 +18,24 @@ def get_consensus(chars, ss):
     return np_count
 
 
-def final_output(chars, count):
-    max_values_per_column = np.max(count, axis=0)
-    print(max_values_per_column)
+def final_output(chars, count_matrix):
+    compare_matrix = np.max(count, axis=0)
+    rows, cols = count_matrix.shape
+    output = []
+
+    def get_char():
+        max_index = np.argmax(col_transpose)
+        max_value = col_transpose[max_index]
+        target_index = np.where(col_transpose == max_value)
+        result = [chars[index] for index in target_index]
+        return '/'.join(result[0])
+
+    for i in range(cols):
+        col = count_matrix[:, i:i + 1]
+        col_transpose = np.transpose(col)[0]
+        output.append(get_char())
+
+    return output
 
 
 if __name__ == '__main__':
@@ -28,6 +43,6 @@ if __name__ == '__main__':
     ss = "ATCGATGC\n" + "GTACaCGC\n" + "ACTACAGC\n" + "CtCAATTC\n" + "CTGgATTC"
 
     count = get_consensus(chars, ss)
-    final_output(chars, count)
+    output = final_output(chars, count)
 
-    # print(count)
+    print(output)
